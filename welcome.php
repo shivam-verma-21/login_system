827289d69e2cf4856f879910ea9b1918d5dc11d4
Shivam Verma <shivamverma.cse21@gmail.com>
2025-11-26 23:24:29 +0530

    Include navbar in welcome.php
    navbar including.

--- welcome.php
+++ welcome.php
@@ -16,6 +16,9 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   </head>
   <body>
+         <?php 
+    require 'partials/_nav.php';
+    ?>
     Welcome - <?php echo $_SESSION['name']?>
   
 
