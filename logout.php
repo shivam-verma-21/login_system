4266e7bda0547883dc6557db5362a0e02589a48c
Shivam Verma <shivamverma.cse21@gmail.com>
2025-11-26 23:23:10 +0530

    Enhance logout.php with re-login prompt
    Add a message and button for re-login after logout.

--- logout.php
+++ logout.php
@@ -4,5 +4,10 @@ session_unset();
 session_destroy(); 
 
 header("location: login.php"); 
+echo "<br>";
+echo '<div class="container my-4">
+        <h1 class="text-center">Please click here for again login </h1>
+</div>';
+ echo '<a href = "/login_system/login.php"><button>Login</button><a>';
 exit;
 ?>
